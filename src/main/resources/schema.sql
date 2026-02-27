b773872b67914f2d14bc3e97497ab692abe5c949
athulkrishna <athulkrishna@m2hinfotech.com>
2026-02-27 16:02:13 +0530

    rebase to mohre_middleware folder

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,56 @@
+-- TABLE 1
+IF OBJECT_ID('Mohre_Company_Employer', 'U') IS NULL
+CREATE TABLE Mohre_Company_Employer (
+
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+
+    Emirates_Id VARCHAR(255),
+    Person_Code VARCHAR(255),
+    Company_Code VARCHAR(255),
+    License_No VARCHAR(255),
+    License_Place VARCHAR(255),
+    License_Place_Arabic VARCHAR(255),
+    Company_Name VARCHAR(255),
+    Company_Name_Arabic VARCHAR(255),
+    Owner_Name VARCHAR(255),
+    Owner_Name_Arabic VARCHAR(255),
+    Owner_Consent VARCHAR(255),
+    Registering_Person_Name VARCHAR(255),
+    Registering_Person_Mobile VARCHAR(255),
+    Registering_Person_EIDA VARCHAR(255),
+    Uab_Status VARCHAR(255),
+    Mohre_Status VARCHAR(255),
+    Registration_No VARCHAR(255)
+);
+
+
+-- TABLE 2
+IF OBJECT_ID('Mohre_Company_Employer_WF', 'U') IS NULL
+CREATE TABLE Mohre_Company_Employer_WF (
+
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+
+    Emirates_Id VARCHAR(255),
+    Person_Code VARCHAR(255),
+    Company_Code VARCHAR(255),
+    License_No VARCHAR(255),
+    License_Place VARCHAR(255),
+    License_Place_Arabic VARCHAR(255),
+    Company_Name VARCHAR(255),
+    Company_Name_Arabic VARCHAR(255),
+    Owner_Name VARCHAR(255),
+    Owner_Name_Arabic VARCHAR(255),
+    Owner_Consent VARCHAR(255),
+    Registering_Person_Name VARCHAR(255),
+    Registering_Person_Mobile VARCHAR(255),
+    Registering_Person_EIDA VARCHAR(255),
+    Uab_Status VARCHAR(255),
+    Mohre_Status VARCHAR(255),
+    Registration_No VARCHAR(255),
+
+    First_User_Action_Date DATETIME2 NULL,
+    Second_User_Action_Date DATETIME2 NULL,
+    First_Approver VARCHAR(255),
+    Second_Approver VARCHAR(255),
+    Is_Rejected VARCHAR(50)
+);
